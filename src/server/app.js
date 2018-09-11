b70f757a23cc8f78afb9ee2f5d731f94f167cc3f
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2018-09-11 16:14:53 +0100

    Added Login/Register functionality using passport

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const Discogs = require('../server/modules/discogs.js');
 const Vinyl = require('./models/vinyl');
+const User = require('./models/user');
 
 const app = express();
 const token = 'jWpfVnLlYPqruBobYHpgftCanMPOzkDXRgkymMlN'; // User token for DiscogsAPI
@@ -12,13 +15,60 @@ app.set('views', './dist/views');
 app.use(express.static('/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(require('express-session')({
+  secret: 'my name is heman',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 mongoose.connect('mongodb://localhost/vinyl_collection', { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/', (req, res) => {
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({ username: req.body.username });
+
+  User.register(newUser, req.body.password, (err, user) => { // eslint-disable-line
+    if (err) {
+      console.log(err);
+      return res.redirect('register');
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('new');
+    });
+  });
+});
+
+app.get('/login', (req, res) => {
+  res.render('index');
+});
+
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: 'new',
+    failureRedirect: 'login',
+  }));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/new', (req, res) => res.render('new'));
+
+app.post('/new', (req, res) => {
   const params = {
     title: req.body.title,
     artist: req.body.artist,
